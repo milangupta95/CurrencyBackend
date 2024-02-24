69594a948124754b01b9418dff59c739a8d6db9d
Milan Kumar Gupta <73779343+milangupta95@users.noreply.github.com>
2024-02-24 15:05:48 +0530

    Update:currencyExchangeController.js

--- controller/currencyExchangeController.js
+++ controller/currencyExchangeController.js
@@ -2,21 +2,22 @@ const coinApi = require("../config/axiosconfig");
 
 module.exports.currenyExchange = async (req, res) => {
     try {
+        // res of each coin from API
         const { fromCurrency, toCurrency, date } = req.body;
         const res_of_from = await coinApi.get(`/coins/${fromCurrency}/history?date=${date}`);
         const res_of_to = await coinApi.get(`/coins/${toCurrency}/history?date=${date}`);
 
+        // Assuming BTC as Base coin converting both to btc and then getting exhchanged Price
         const curr_price_from_in_btc = res_of_from.data.market_data.current_price.btc;
         const curr_price_to_in_in_btc = res_of_to.data.market_data.current_price.btc;
-
-        console.log(curr_price_from_in_btc + " " + curr_price_to_in_in_btc);
-
         const exchanged_price = (curr_price_from_in_btc * 1.0) / curr_price_to_in_in_btc;
-        console.log(exchanged_price);
+
+        // sending response
         res.status(200).json({
             exchanged_price: exchanged_price
         })
     } catch (err) {
+        // response when error occured
         res.status(500).json({
             message: err.message
         })
